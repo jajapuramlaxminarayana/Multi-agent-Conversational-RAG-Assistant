b7467c924dc4178f43f474c456f3ae79a085e06a
jajapuramlaxminarayana <jajapuramlaxminarayana@gmail.com>
2026-09-25 08:19:56 +0530

    Add agents/summarizer_agent.py

---
+++ agents/summarizer_agent.py
@@ -0,0 +1,36 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+def summarizer_agent(text):
+
+    prompt = f"""
+    Summarize this research paper.
+
+    Include:
+    - Objective
+    - Methodology
+    - Results
+    - Future Scope
+
+    Text:
+    {text}
+    """
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
